4a820148b73e19315ce9d65004bf7ae4b4373f9a
zhayongchun <zhayongchun@meituan.com>
2024-07-05 21:32:06 +0800

    Allow test

--- src/test/java/zoz/cool/apihub/ApiHubApplicationTests.java
+++ src/test/java/zoz/cool/apihub/ApiHubApplicationTests.java
@@ -1,13 +1,13 @@
-//package zoz.cool.apihub;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ApiHubApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package zoz.cool.apihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
